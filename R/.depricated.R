130eab30b21daffb56c19904e4e1c03b25633136
Trinh Dong Huu Khanh <trinhdhk@oucru.org>
2019-12-02 12:17:00 +0700

    Depricated function

--- R/.depricated.R
+++ R/.depricated.R
@@ -1,36 +1,30 @@
+
+
+
 ._lik.ratio_summary <- function(fit, stat_digits=2, p_digits=4, verbose = FALSE){
+  any.ia <- any(attr(terms(formula(fit)),"order")>1)
+  if (any.ia){
+    message('Interaction terms detected. Using the explicit form.')
+    return(._lik.ratio_summary(explicit(fit), stat_digits = stat_digits, p_digits = p_digits, verbose = verbose))
+  }
+
   est <- coef(fit)
-  ci <- confint(fit)
+  ci <- suppressMessages(confint(fit))
   result <- data.frame(log.OR = est, OR = exp(est),
                        lower.CI = exp(ci[,1]), upper.CI = exp(ci[,2]))
 
   has.intercept <- (names(coef(fit))[1]=="(Intercept)")
-  any.ia <- any(attr(terms(formula(fit)),"order")>1)
   lr.test <- data.frame(drop1(fit,test="Chisq"))[-1,c("Df","Pr..Chi.")]
-  if (any.ia){
-    lr.test <- data.frame(drop1(explicit(fit), test='Chisq'))[-1,c("Df","Pr..Chi.")]
-    rownames(lr.test) <-
-      gsub('\\s{1}\\*\\s{1}', ':',
-           gsub('(^I\\()|(\\)$)', '', rownames(lr.test), perl = TRUE),
-           perl = TRUE)
-  }
 
-  browser()
-  if (has.intercept) {
-    p.value <-  lr.test[,"Pr..Chi.", drop = FALSE]
-    names(p.value) <- 'p.value'
-    row_order <- factor(rownames(result), levels = rownames(result))
-    result <- merge(result, p.value, all.x = TRUE, by='row.names')
-    result$Row.names <- factor(result$Row.names, levels = row_order)
-    result <- dplyr::arrange(result, Row.names)
-    rownames(result) <- result$Row.names
-    result <- result[, -1]
-  }
+  if (has.intercept) result$p.value <- c(NA,lr.test[,"Pr..Chi."])
 
   for (i in 1:4) result[,i] <- formatC(result[,i], digits = stat_digits, format = 'f')
   if (length(result$p.value)) result$p.value <- formatC(result$p.value, p_digits, format = 'f')
   result$p.value <- ifelse(grepl('NA', result$p.value), '', result$p.value)
   if (naprint(fit$na.action)!="") message("\nNote:",naprint(fit$na.action))
-  if (verbose) return(print(result))
+  if (verbose) {
+    cat("\nNote: 95% confidence intervals and p-values are based on likelihood ratio statistics.\n\n")
+    if (!has.intercept) cat("\nNote: p-values of LR-tests not calculated because model has no intercept.\n\n")
+  }
   return(result)
 }
